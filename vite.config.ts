9fc6ef5344c9288147deb094b3693a57b4a8992f
Anwarhacker <patelanwar647@gmail.com>
2026-01-10 18:35:28 +0530

    check now

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
       host: '0.0.0.0',
       proxy: {
         '/api': {
-          target: 'https://video-website-gamma.vercel.app',
+          target: 'https://video-website-rxsu.vercel.app/',
           changeOrigin: true,
         }
       }
